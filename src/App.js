8f114e353b73430707e75199071ef8022b63dae9
José Ignacio <joseignacioig9910@gmail.com>
2022-11-03 23:42:48 +0100

    😓🔫

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 function App() {
+  const [countriesList, setCountriesList] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(() => {
+    setIsFetching(true);
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => {
+        console.log(response.data);
+        setCountriesList(response.data);
+        setIsFetching(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (isFetching === true) {
+    return <h3>... looking for</h3>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navbar />
+      <div className="container">
+          <CountriesList countriesList={countriesList} />
+          <Routes>
+            <Route
+              path="/:id"
+              element={<CountryDetails countriesList={countriesList} />}
+            />
+          </Routes>
+        </div>
+      </div>
   );
 }
 
---
+++ src/components/CountryDetails.jsx
@@ -0,0 +1,14 @@
+import { Link, useParams } from 'react-router-dom';
+
+function CountryDetails(props) {
+
+//No doy para más, este finde vuelvo al lab...
+
+  return (
+    <div className="col-7">
+      
+    </div>
+  );
+}
+
+export default CountryDetails;
